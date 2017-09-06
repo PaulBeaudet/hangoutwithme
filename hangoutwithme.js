01c0e00daec838e4865f8c42ce9d1b25bb61ea16
Paul Beaudet <inof8or@gmail.com>
2017-09-06 15:33:40 -0400

    Create dynamic routes to lobby rooms

--- hangoutwithme.js
+++ hangoutwithme.js
@@ -2,15 +2,36 @@
 // Serves webpage routes to virtual lobbies for visitors to schedule a google hangout with owner of lobby
 var path = require('path'); // cause its nice to know where things are
 
+var lobby = {
+    room: [],             // Array of active lobbies that can be visited
+    create: function(lobbyname, onCreate){
+        for(var i = 0; i < lobby.room.length; i++){
+            if(lobbyname === lobby.room[i]){
+                if(onCreate){onCreate('Room taken');} // error name taken case
+                return false;
+            }
+        }
+        lobby.room.push('/'+lobbyname);
+        if(onCreate){onCreate();}                     // callback success case with no error
+        return true;
+    }
+};
+
 var route = {
-    userNotFound: function(){
-        return function(req, res){
-            res.sendFile(path.join(__dirname+'/notFound.html'));
-        };
-    },
     about: function(){
         return function(req, res){
-            res.send('this is a site for setting up times to video chat with people you know or want to get to know');
+            res.sendFile(path.join(__dirname+'/public/welcome.html'));
+        };
+    },
+    findUser: function(){
+        return function(req, res){
+            for(var i = 0; i < lobby.room.length; i++){
+                if(lobby.room[i] === req.url){
+                    res.sendFile(path.join(__dirname+'/public/lobby.html'));
+                    return;
+                }
+            }
+            res.sendFile(path.join(__dirname+'/public/notFound.html'));      // if a user can not be found
         };
     }
 };
@@ -18,18 +39,19 @@ var route = {
 var serve = {                                                // handles express server setup
     express: require('express'),                             // server framework library
     parse: require('body-parser'),                           // middleware to parse JSON bodies
-    theSite: function (){                                    // methode call to serve site
-        var app = serve.express();                           // create famework object
-        var http = require('http').Server(app);              // http server for express framework
-        app.use(serve.parse.json());                         // support JSON bodies
-        app.use('/static', serve.express.static(path.join(__dirname, 'public'))); // serve static files for site resources
-        var router = serve.express.Router();                 // create express router object to add routing events to
-        router.get('/', route.about());                      // Quick about page for those that are lost
-        router.get('/*', route.userNotFound());              // Default route goes to a user not found page
-        app.use(router);                                     // get express to user the routes we set
+    theSite: function(){                                     // method call to serve site
+        serve.app = serve.express();                         // create famework object
+        var http = require('http').Server(serve.app);        // http server for express framework
+        serve.app.use(serve.parse.json());                   // support JSON bodies
+        serve.app.use('/static', serve.express.static(path.join(__dirname, 'public'))); // serve static files for site resources
+        serve.router = serve.express.Router();               // create express router object to add routing events to
+        serve.router.get('/', route.about());                // Quick about page for those that are lost
+        serve.router.get('/*', route.findUser());            // Default route goes to a user not found page
+        serve.app.use(serve.router);                         // get express to user the routes we set
         return http;
     }
 };
 
 var http = serve.theSite();                                  // set express middleware and routes up
 http.listen(process.env.PORT);                               // listen on specified PORT enviornment variable
+lobby.create('paulbeaudet');
---
+++ readme.md
@@ -0,0 +1,5 @@
+Serves webpage routes to virtual lobbies for visitors to schedule a Google hangout with owner of lobby
+
+### Copyright
+2017 Paul Beaudet
+MIT License
