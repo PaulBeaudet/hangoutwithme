ef234920986ebbdf73e9a46ef08d0aaa9066f98a
Paul Beaudet <inof8or@gmail.com>
2017-09-22 13:00:57 -0400

    reorg somethings and test push in heroku

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,30 +1,44 @@
 // servicesWorker.js ~ Copyright 2017 ~ Paul Beaduet ~ MIT LICENSE
 
-self.onnotificationclick = function(onClick) {onClick.notification.close();};
-
-var openHangout = false; // store whether hangout is opened on notification click
-
-self.addEventListener('message', function(event) {
-    if (event.data.command == "setPush") {
-        self.onnotificationclick = function(onClick) {
-            onClick.notification.close();
-            clients.openWindow(event.data.hangoutLink);
-            openHangout = true;
-        };
-        var warningTime = 60000; // give a minute warning
-        var currentTime = new Date().getTime();
-        var timeToFire = event.data.appointment - currentTime;
-        if(timeToFire > warningTime){timeToFire = timeToFire - warningTime;}
-        else {warningTime = 0;} // given chat is comming up quickly
-        console.log('version 1');
-        setTimeout(function sendNotification(){
-            if(warningTime){
-                self.registration.showNotification('hangout', {body: event.data.hangoutLink});
+var fromClient = {      // comunications that come from client
+    openHangout: false, // store whether hangout is opened on notification click
+    init: function(){
+        self.onnotificationclick = function(onClick) {onClick.notification.close();};
+        self.addEventListener('message', function(event) {
+            if (event.data.command == "setPush") {
+                self.onnotificationclick = function(onClick) {
+                    onClick.notification.close();
+                    clients.openWindow(event.data.hangoutLink);
+                };
+                var warningTime = 15000; // give a minute warning
+                var currentTime = new Date().getTime();
+                var timeToFire = 30000;
+                // var timeToFire = event.data.appointment - currentTime;
+                if(timeToFire > warningTime){timeToFire = timeToFire - warningTime;}
+                else {warningTime = 0;} // given chat is comming up quickly
+                setTimeout(function sendNotification(){
+                    if(warningTime){self.registration.showNotification('hangout', {body: event.data.hangoutLink});}
+                    setTimeout(function openHangout(){
+                        if(fromClient.openHangout){}
+                        else{clients.openWindow(event.data.hangoutLink);}
+                    }, warningTime);
+                }, timeToFire);// set to show on the dot x millis from now
             }
-            setTimeout(function openHangout(){
-                if(openHangout){}
-                else{clients.openWindow(event.data.hangoutLink);} // just open hangout like a boss
-            }, warningTime);
-        }, timeToFire);// set to show on the dot x millis from now
+        });
+    },
+};
+
+/*var socket = { // Communications that come from server
+    io: io(),  // hopefully this pulls dependancies from the same server
+    init: function(){
+        socket.io.on('notification', socket.notify());
+    },
+    notify: function(){
+        return function(data){
+            self.registration.showNotification(data.title, {body: data.body});
+        };
     }
-});
+}; */
+
+fromClient.init();
+//socket.init();
